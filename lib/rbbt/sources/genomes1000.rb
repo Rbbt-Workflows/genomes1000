d914ec9edf963276f82168ae19f44c1e8d5a9442
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-02-13 20:04:54 +0100

    nil

--- lib/rbbt/sources/genomes1000.rb
+++ lib/rbbt/sources/genomes1000.rb
@@ -30,22 +30,43 @@ module Genomes1000
     nil
   end
 
-  Genomes1000.claim Genomes1000.mutations_hg18, :proc do
+  Genomes1000.claim Genomes1000.mutations_hg18, :proc do |filename|
     require 'rbbt/sources/organism'
+    Workflow.require_workflow "Sequence"
 
-    hg19_tsv = Genomes1000.mutations.tsv :unnamed => true
+    Sequence.swap_build Genomes1000.mutations, filename, Genomes1000.organism, "Hsa/may2009"
+    nil
 
-    mutations = hg19_tsv.values
+    #hg19 = Genomes1000.mutations.tsv :type => :list
+    #hg19 = hg19.attach Sequence.job(:liftover, "Genomes1000", :positions => hg19.values, :source => Genomes1000.organism, :target => "Hsa/may2009").run
 
-    translations = Misc.process_to_hash(mutations){|mutations| Organism.liftOver(mutations, Genomes1000.organism, "Hsa/may2009")}
+    #tsv = hg19.reorder("Hsa/may2009", hg19.fields - ["Hsa/may2009", "Genomic Mutation"])
+    #tsv.key_field = "Genomic Mutation"
 
-    tsv = hg19_tsv.process "Genomic Mutation" do |mutation|
-      translations[mutation]
-    end
+    #tsv.to_s
+    #TmpFile.with_file do |source_bed|
+    #  Open.write(source_bed) do |file|
+    #    Genomes1000.mutations.open do |line|
+    #      file.puts line.split("\t")[1].split
+    #    end
+    #  end
+    #end
 
-    tsv.namespace = "Hsa/may2009"
 
-    tsv.to_s
+
+    #hg19_tsv = Genomes1000.mutations.tsv :unnamed => true
+
+    #mutations = hg19_tsv.values
+
+    #translations = Misc.process_to_hash(mutations){|mutations| Organism.liftOver(mutations, Genomes1000.organism, "Hsa/may2009")}
+
+    #tsv = hg19_tsv.process "Genomic Mutation" do |mutation|
+    #  translations[mutation]
+    #end
+
+    #tsv.namespace = "Hsa/may2009"
+
+    #tsv.to_s
   end
 
 end
